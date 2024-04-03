60166b5f008c974c39f152d276fedf7a777d3b41
Sterrrrest <sterrrrest@gmail.com>
2024-04-03 12:06:52 +0300

    Added decorator to Admin Models

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ class Flatinline(admin.TabularInline):
     model = Flat.owned_by.through
     raw_id_fields = ('owner',)
 
-
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ['created_at']
@@ -17,15 +17,12 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ('liked_by',)
     inlines = [Flatinline]
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat',)
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('owner_flats',)
 
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
